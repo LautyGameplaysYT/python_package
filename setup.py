25fb053e77d3b102f8d09456d293a88957e89fe6
LautyGameplaysYT <lautaromarchisone@hotmail.com>
2024-09-14 11:51:35 -0300

    v2

--- setup.py
+++ setup.py
@@ -1,15 +1,8 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name = 'Lauty_utilities',
-    version = '0.0.2',
+    version = '0.0.3',
     author = 'LautyGameplaysYT',
-    author_email= 'redacted@redacted.com',
-    url='https://github.com/LautyGameplaysYT/python_package',
-    project_urls = {
-        "Bug Tracker": "https://github.com/LautyGameplaysYT/python_package/issues"
-    },
-    license= "GNU-3.0",
-    packages= ['Lauty_utilities'],
-    install_requires = [],
+    packages= find_packages(),
 )
